edbacbd6df29cdbafccd32fff2f999dcdfefc90c
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-10-17 16:43:24 -0400

    added textfields and buttons

--- client/src/components/NewMovieForm.js
+++ client/src/components/NewMovieForm.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 export default class movies extends Component {
 
@@ -12,18 +15,48 @@ export default class movies extends Component {
         return (
         <div>
             <form onSubmit={this.handleSumbit}>
-                <label >Movie name: </label>
+            <TextField type="text" name="name" onChange={this.props.handleInputChange}
+        id="outlined-uncontrolled"
+        label="Movie Name"
+        value={this.props.newMovie.movie}
+           margin="normal"
+        variant="outlined"
+      />
+       <TextField
+        id="outlined-uncontrolled"
+        label="Year"
+        value={this.props.newMovie.year}
+        type="text" 
+        name="year"  onChange={this.props.handleInputChange}
+        margin="normal"
+        variant="outlined"
+      />
+       <TextField
+        id="outlined-uncontrolled"
+        label="Image URL"
+        value={this.props.newMovie.imageLink}
+        type="text" name="imageLink" onChange={this.props.handleInputChange}
+        margin="normal"
+        variant="outlined"
+      />
+      <div class="newmoviebutton">
+       <Button variant="contained" type="submit">
+        Submit
+      </Button>
+      </div>
+
+                {/* <label >Movie name </label>
                 <input type="text" name="name" onChange={this.props.handleInputChange}
                     value={this.props.newMovie.movie}
                 />
-                <label>Year: </label>
+                <label>Year </label>
                 <input  type="text" 
                     name="year"  onChange={this.props.handleInputChange} value={this.props.newMovie.year}
                     />
-                <label >Image :</label>
+                <label >Image</label>
                 <input type="text" name="imageLink" onChange={this.props.handleInputChange}  value={this.props.newMovie.imageLink}
-                />
-                 <input type="submit" value="Add"/> 
+                /> */}
+                 {/* <input type="submit" value="Add"/>  */}
             </form>
         </div>
         );
