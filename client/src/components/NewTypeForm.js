3c80a838a255faf69fc92f9f3abdd59fbc735677
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-10-17 16:42:47 -0400

    added textfields

--- client/src/components/NewTypeForm.js
+++ client/src/components/NewTypeForm.js
@@ -1,29 +1,41 @@
 import React, { Component } from 'react';
-
-
+import { makeStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 export default class NewTypeForm extends Component {
     render() {
         return (
         <div>
             <form onSubmit={this.props.handleAddNewForm}>
-                <label >Type Name:</label>
-                <input 
-                    id="type-name"
-                    type="text"
-                    name="name"
-                    onChange={this.props.handleInputChange}
-                    value={this.props.newType.name}
-                />
-                <label htmlFor="image-link">Image:</label>
-                <input 
-                    id="image-link"
-                    type="text"
-                    name="imageLink"
-                    onChange={this.props.handleInputChange}
-                    value={this.props.newType.imageLink}
-                    />
+
+            <TextField
+        id="outlined-uncontrolled"
+        label="Name"
+        type="text"
+        name="name"
+        onChange={this.props.handleInputChange}
+        value={this.props.newType.name}
+     
+        margin="normal"
+        variant="outlined"
+      />
+      <TextField
+        id="outlined-uncontrolled"
+        label="Image URL"
+        type="text"
+        name="imageLink"
+        onChange={this.props.handleInputChange}
+        value={this.props.newType.imageLink}
+        margin="normal"
+        variant="outlined"
+      />
+               <div class="newtypebutton">
+                   <Button variant="contained" type="submit">
+                       Submit
+                   </Button>
+               </div>
                    
-                <input type="submit" value="Submit" />
             </form>
         </div>
         );
