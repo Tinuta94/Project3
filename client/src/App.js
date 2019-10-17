136c18596a80a5b143f71aa0f9419c72cfafc852
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-10-17 17:01:58 -0400

    changed the name

--- client/src/App.js
+++ client/src/App.js
@@ -10,23 +10,20 @@ import Tab from '@material-ui/core/Tab';
 function App() {
   return (
     <div className="App">
-       {/* <header>
-       <h1> Welcome To Atlantic</h1>
-         </header> */}
        
       <Router>
       <AppBar position="static" style={{backgroundColor: "brown"}}>
         <Tabs value="" onChange="" aria-label="simple tabs example" >
         
         <Link to="/" class="link"><Tab label="Home" /></Link>
-       {/* <Link to="/"><i class="fas fa-home"></i></Link> */}
+    
 
           <Link ><Tab label="" /></Link>
           <Link ><Tab label="" /></Link>
           <Link ><Tab label="" /></Link>
          
          
-       <h3>Welcome To Atlantic</h3>
+       <h1>My Movie World</h1>
           
         </Tabs>
       </AppBar>
